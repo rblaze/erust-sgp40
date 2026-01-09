9256ef52e2b2bb09f4d82672e3ba252c142196dd
Andrey Sverdlichenko <blaze@rusty.zone>
2026-01-08 23:26:57 -0500

    Fix error in command code

--- src/sgp40/commands.rs
+++ src/sgp40/commands.rs
@@ -3,4 +3,4 @@ use crate::sensirion::Cmd;
 pub const GET_SERIAL_NUMBER: Cmd = [0x36, 0x82];
 pub const TURN_HEATER_OFF: Cmd = [0x36, 0x15];
 pub const EXECUTE_SELF_TEST: Cmd = [0x28, 0x0e];
-pub const MEASURE_RAW_SIGNAL: Cmd = [0x26, 0x02];
+pub const MEASURE_RAW_SIGNAL: Cmd = [0x26, 0x0F];
